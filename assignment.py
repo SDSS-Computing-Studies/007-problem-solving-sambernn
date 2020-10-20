2139392e43bd070ca42c1bc021a2c3f140d361b8
sambernn <55156541+sambernn@users.noreply.github.com>
2020-10-20 13:02:12 -0700

    ss

--- assignment.py
+++ assignment.py
@@ -1,2 +1,30 @@
+#game: https://www.clickerheroes.com/play.html
+
 import time as t
 import pyautogui as p
+
+def clicker():
+    #locate monster and click
+    myList = p.locateCenterOnScreen("shop.png", grayscale=True, confidence=0.8)
+    print(myList)
+    p.moveTo(myList[0],myList[1] - 200)
+    p.click(clicks=30, interval = 0.1)
+    
+def upgrade():
+    #purchase upgrades once apropriate amount of gold is made
+    pass
+
+def switch():
+    #switch stage after beating monster a certain number of times
+    pass
+
+def fail():
+    #go back a stage if cannot beat boss
+    pass
+
+def main():
+    clicker()
+    pass
+
+clicker()
+
